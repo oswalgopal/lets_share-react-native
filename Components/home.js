c023197c30d74a833fb80daf48e2e757f2f43ad5
Gopal Oswal <gopal@kritin.in>
2019-12-05 00:25:57 +0530

    added basic component

---
+++ Components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+export default class HomePage extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <Text>This is my home page</Text>
+        )
+    }
+}
